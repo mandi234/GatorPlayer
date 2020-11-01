acd6129e1e750717ead66d021761d1b4782f9285
kamilmandas <kamil.mandas@gmail.com>
2020-11-01 21:19:48 +0100

    Refactor packeges and introduce song class

---
+++ app/src/main/java/com/example/gatorplayer/model/Song.java
@@ -0,0 +1,49 @@
+package com.example.gatorplayer.model;
+
+public class Song {
+        private String songName;
+        private int duration;
+        private String genre;
+        private String artistName;
+        private String producerName;
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getProducerName() {
+        return producerName;
+    }
+
+    public void setProducerName(String producerName) {
+        this.producerName = producerName;
+    }
+}
